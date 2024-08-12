726291db43ad82bb461c6ed438eef9e483609d95
Gagan Vishwakarma <gvishwakarma@qodeleaf.com>
2024-08-12 21:51:26 +0530

    ignition folder removed

--- ignition/modules/Token.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-const TokenModule = buildModule("TokenModule", (m) => {
-  const token = m.contract("Token",["Coins", "CNS",8,1000]);
-
-  return { token };
-});
-
-module.exports = TokenModule;
